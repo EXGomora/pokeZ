0b3a9d8a1617a12086cd926cf2a870a71eeeae38
EXGomora <dusttostardust@protonmail.com>
2024-03-26 01:20:37 -0400

    Newbox: Fixed wrong text_decimal for lucky number
    https://github.com/fellowship-of-the-roms/pokecrystal/commit/44f69898540bec160f261f7b4179a499c0a8f1a0

--- data/text/common_2.asm
+++ data/text/common_2.asm
@@ -1033,7 +1033,7 @@ _KarpGuruRecordText::
 	text "CURRENT RECORD"
 
 	para "@"
-	text_decimal wBufferMonID, 2, 5
+	text_ram wStringBuffer1
 	text " caught by"
 	line "@"
 	text_ram wMagikarpRecordHoldersName
@@ -1049,7 +1049,7 @@ _LuckyNumberMatchPartyText::
 	line "with the ID number"
 
 	para "of @"
-	text_ram wStringBuffer2
+	text_decimal wBufferMonID, 2, 5
 	text " in"
 	line "your party."
 	prompt
